f8e0bde3a512a3fb80f558ffd611a8fcd8ddcf4f
fireflies001 <joellloydFL@gmail.com>
2021-01-30 20:15:30 +0800

    change app js

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@ var VERSION = "1.0.0";
 var app = angular.module('myApp', ['ngRoute']);
 app.config(function($routeProvider) {
     $routeProvider.when("/", {
-        templateUrl : "/views/login/login.html",
+        templateUrl : "./views/login/login.html",
         controller : 'loginController'
     })
     .when("/dashboard", {
-        templateUrl : "/views/dashboard/dashboard.html",
+        templateUrl : "./views/dashboard/dashboard.html",
         controller : 'dashboardController'
     })
     .otherwise({redirectTo: '/'});
